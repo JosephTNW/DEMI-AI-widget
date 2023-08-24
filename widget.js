94c6ed4fc2c2117b549e4f052138a415c4ff6836
rynhan <hanzr250@gmail.com>
2023-08-24 08:40:01 +0700

    -

--- widget.js
+++ widget.js
@@ -30,7 +30,7 @@ function toHyperlink(txt) { // to find URLs within the string and convert them i
 }
 
 // Default bot image URL, replace this with the actual URL of your bot's image
-const botImageUrl = "https://relieved-top-coat-toad.cyclic.cloud/D3.png";
+const botImageUrl = "https://raw.githubusercontent.com/rynhan/DEMI-AI-widget/main/D3.png";
 const createChatLi = (message, className, imageUrl = botImageUrl) => {
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", className);
